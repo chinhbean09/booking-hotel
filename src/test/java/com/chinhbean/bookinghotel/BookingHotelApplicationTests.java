3bc859a360eee4e5b9e9a9a308717803591b5b76
chinhbean09 <chinh0726@gmail.com>
2024-05-15 00:01:01 +0700

    Build source code v.0.1

---
+++ src/test/java/com/chinhbean/bookinghotel/BookingHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinhbean.bookinghotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
